d25423ff271d3fb1ebae6b3d4f87a613aa4b5c02
FabricioMontanari <fabriciomontanariv@gmail.com>
2024-11-20 20:54:23 -0600

    api y web corriendo

--- Práctica con errores/Persona.WEB/Web/Pages/Cuenta/Perfil.cshtml.cs
+++ Práctica con errores/Persona.WEB/Web/Pages/Cuenta/Perfil.cshtml.cs
@@ -52,5 +52,7 @@ namespace Web.Pages.Cuenta
             DocumentoContenido documento = new DocumentoContenido() { Nombre = archivo.FileName, Contenido = contenido };
             return documento;
         }
+
+
     }
 }
